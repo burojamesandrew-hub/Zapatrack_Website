cedae0b548b2d517f0a9a3007b11baec8483942c
femtowhahaha <k.chavez9985@gmail.com>
2026-05-21 05:18:41 +0800

    LAST

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,7 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      jsqr: path.resolve(__dirname, 'node_modules/jsqr/dist/jsQR.js'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
-    }
-  }
+  base: '/Zapatrack_Website/',
 })
